ab9b38fe44f83f3f28662785bddaca3dadcc98a4
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-02-24 15:28:58 +0530

    js start lec 1

---
+++ lec1/lec1.js
@@ -0,0 +1,43 @@
+console.log("Hi Java")
+let a=7;
+console.log(a)
+
+
+let nam1e="babbarr";
+console.log(nam1e)
+
+console.log(5!=4)
+
+
+let age=18; 
+let status=(age>=18)?"can vote":"can't vote";
+console.log(status)
+
+let marks=-8;
+if(marks>=90){
+    console.log("A");
+}
+
+else if(marks>=80){
+    console.log("B")
+}
+
+else{
+    console.log("fAIl")
+}
+
+let number=2;
+switch(number){
+case 1: console.log('A');
+break;
+case 2: console.log('B');
+break;
+case 3: console.log('C');
+break;
+default : console.log('D');
+break;
+}
+
+for(let i=0;i<5;i++){
+    console.log(i);
+}
