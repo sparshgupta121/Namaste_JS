3c7b252586928e6899b6489eb8d2b463592f4a4b
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-04-10 14:52:05 +0530

    dataa type conversion

---
+++ Datatypeconversion/datatypeconverson.js
@@ -0,0 +1,24 @@
+ let score= "33" 
+console.log(typeof score);
+
+//  const {scoree}=req.body
+
+
+let valueinnum = Number(score)
+console.log(typeof valueinnum);
+
+
+//"33" => 33
+
+//"33abc"=> Nan
+// true =>1; false =>0
+
+let isLoggedIn = " abjs"
+let booleanisLoggedin = Boolean(isLoggedIn)
+console.log(booleanisLoggedin);
+
+//1=>true ; 0=>false
+//""=>false if empty if add space it show result as true
+//" sparsh " => true
+
+let num= 33
