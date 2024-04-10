22a821f8fc7771a78b8ff466cb54938f856cacd4
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-04-10 15:41:40 +0530

    operations

---
+++ 04 Operation/operations.js
@@ -0,0 +1,26 @@
+let value=3
+let newvalu= value+50
+console.log(newvalu); 
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*9);
+console.log(7/3);
+console.log(4%3);
+
+//listed below is not good ways to define or perform operations
+ 
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
