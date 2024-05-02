4629c824a9e1eb9b8694ce7889ae19c0e8f1f264
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-05-02 11:05:41 +0530

    array part 1

---
+++ Array/array.js
@@ -0,0 +1,34 @@
+const myArr=[0,1,4,2,3,5,8,5]
+console.log(myArr[0]);
+
+const myHeros = new Array("Shaktiman" , "Naagraj", "Chota Bheem")
+
+console.log(myHeros.push(6));
+//push add element at last index
+console.log(myHeros);
+
+myHeros.unshift(9)
+//unshift add element at index 0
+//shift delete element at index 0
+
+console.log(myHeros);
+
+console.log(myArr.indexOf(4));
+
+const newarrr = myArr.join()
+//join will convert the array to string 
+console.log(myArr);
+console.log(newarrr);
+console.log(typeof(newarrr));
+
+
+
+
+
+
+
+
+
+
+
+
