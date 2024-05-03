5908ec34b5f507b3be5711c5dd11fe21184d2633
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-05-03 15:57:46 +0530

    arrayss-lec2

--- Array/array.js
+++ Array/array.js
@@ -21,14 +21,39 @@ console.log(myArr);
 console.log(newarrr);
 console.log(typeof(newarrr));
 
+//Array lec 2
+
+const marvelHeros = ["thor","Ironman","spiderman"]
+const dcHeros= ["superman","baatman","Flash"]
+
+// marvelHeros.push(dcHeros);
+
+/* Push Modifies the existing array while concatinate returns a new array which we have to store in a new varriable, concat doesn't modify the existing array */
+
+console.log(marvelHeros);
+
+const allheross= marvelHeros.concat(dcHeros)
+
+console.log(allheross);
+
+//easy method to  joinn use spread methods
+
+const all_neww_herossss= [...dcHeros,...marvelHeros]
+
+console.log(all_neww_herossss);
 
 
+//flat is used to take array into array into array to a single array.
 
+console.log( Array.isArray("Sparsh") );
+console.log(Array.from("Sparsh"));
 
+console.log(Array.from({name:"Sparsh"})); //Will create an empty array because not defined that what to use for creating array ie..key or value?   //can be asked in interviews //later on we can create array using key or values when we create projects  
 
+let score1= 100
+let score2=200
+let score3=300
 
-
-
-
+console.log(Array.of(score1,score2,score3));
 
 
