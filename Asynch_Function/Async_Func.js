e675c680adeb5f467e5c50e0215bfeb698717899
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-06-12 00:17:16 +0530

    Asynch Function

---
+++ Asynch_Function/Async_Func.js
@@ -0,0 +1,35 @@
+/* what is asynch code, how it is available , where it will work --->In this Seesion
+
+help in studyingpromise asnc-await  
+
+Javascript is a Synchronous language
+exception when we include some extra thingss
+
+JS is a Slow language but it doesn't make you feel like that bcoz of delegating things and js engine 
+
+js engine is always available with run time enviorment
+
+run time enviorment can be of browser or node
+
+*/
+
+/* Execution Contextt
+
+Execute one line of code at a time 
+Each Operation waits for the last one to complete before executing
+
+
+        Call Stack          Memory Heap
+
+*/
+
+/* Blocking code        vs              Non Blocking Code
+
+Block the flow of the program          Doesn't block Execution
+
+Read File Sync                         Read File Async
+
+
+
+
+*/
